113234291741167c203854b6b03570d6723314e0
Ian Mitchell <56634530+iangmitchell@users.noreply.github.com>
2024-12-10 09:57:37 +0000

    Add files via upload

---
+++ react/my-app1/App.js
@@ -0,0 +1,13 @@
+import Header from './Header.js';
+import List from './List.js';
+
+function App () {
+    return(
+    <>
+        <Header />
+        <List />
+    </>
+    );
+}
+
+export default App;
---
+++ react/my-app1/List.js
@@ -0,0 +1,14 @@
+function List(){
+    return (
+        <div class="notification is-primary">
+            <ol type="i">
+                <li> Carrots </li>
+                <li> Broccoli</li>
+                <li> Brussels</li> 
+            </ol>
+        </div>
+        
+    );
+}
+
+export default List;
